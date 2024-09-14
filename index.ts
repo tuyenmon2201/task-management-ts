c5d9d02f5065eea7d61e1ff89f1a54c36d0bf007
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-15 00:48:45 +0700

    Install CORS

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser = require("body-parser");
+import cors from "cors";
 dotenv.config();
 
 import { connectDatabase } from "./config/database";
@@ -14,6 +15,15 @@ import { routesApi } from "./routes/client/index.route";
 app.use(bodyParser.json());
 
 routesApi(app);
+
+app.use(cors());
+
+// const corsOptions = {
+//   origin: 'http://abc.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
+// app.use(cors(corsOptions));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
