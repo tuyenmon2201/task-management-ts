ad43c185e7eed37c24d25746b7b7a1c0d7718eeb
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-15 13:36:15 +0700

    Login

--- controllers/client/user.controller.ts
+++ controllers/client/user.controller.ts
@@ -44,3 +44,35 @@ export const register = async (req: Request, res: Response) => {
         });
     }
 }
+
+export const login = async (req: Request, res: Response) => {
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if(!user){
+        res.json({
+            code: 400,
+            message: "Email không tồn tại!"
+        });
+        return;
+    }
+
+    if(md5(password) != md5(user.password)){
+        res.json({
+            code: 400,
+            message: "Sai mật khẩu!"
+        });
+        return
+    }
+
+    res.json({
+        code: 200,
+        message: "Đăng nhập thành công!",
+        token: user.token
+    })
+}
--- routes/client/user.route.ts
+++ routes/client/user.route.ts
@@ -4,4 +4,6 @@ import * as controller from "../../controllers/client/user.controller";
 
 router.post("/register", controller.register);
 
+router.post("/login", controller.login);
+
 export const userRoute = router;
